73593946b7c2270f6d41f2f7cf8e98f9578a74ae
Paul P.H. Wilson <paul.wilson@wisc.edu>
2017-07-12 17:46:11 -0500

    * reuse check-repo * set & reuse install_dir * fix some typos

--- ubuntu_mint.sh
+++ ubuntu_mint.sh
@@ -2,62 +2,66 @@
 # This script contains common code for building PyNE on various Debian-derived systems
 #
 
-function system_update {
+function check_repo() {
+
+    repo_name=$1
+
+    if [ -d $repo_name ] ; then
+        read -p "Delete the existing $repo_name directory and all contents? (y/n) " -n 1 -r
+        if [[ $REPLY =~ ^[Yy]$ ]] ; then
+            rm -rf $repo_name
+        fi
+    fi  
     
-    # Use package manager for as many packages as possible
 }
 
 function build_moab {
 
     # Install MOAB
+    cd $install_dir
     mkdir -p moab
     cd moab
-    if [ -d moab-repo ] ; then
-        read -p "Delete the existing moab-repo directory and all contents? (y/n) " -n 1 -r
-        if [[ $REPLY =~ ^[Yy]$ ]] ; then
-            rm -rf moab-repo
-        fi
-    fi  
+    check_repo moab-repo
     git clone --branch Version4.9.1 --single-branch https://bitbucket.org/fathomteam/moab moab-repo
     cd moab-repo
     autoreconf -fi
-    cd ..
     mkdir -p build
     cd build
-    ../moab/configure --enable-shared --enable-dagmc --with-hdf5=$hdf_libdir --prefix=$HOME/opt/moab
+    ../configure --enable-shared --enable-dagmc --with-hdf5=$hdf5_libdir --prefix=$install_dir/moab
     make
     make install
-    export LD_LIBRARY_PATH=$HOME/opt/moab/lib:$LD_LIBRARY_PATH
-    export LIBRARY_PATH=$HOME/opt/moab/lib:$LIBRARY_PATH
-    echo "export LD_LIBRARY_PATH=$HOME/opt/moab/lib:\$LD_LIBRARY_PATH" >> ~/.bashrc
-    echo "export LIBRARY_PATH=$HOME/opt/moab/lib:\$LIBRARY_PATH" >> ~/.bashrc
-    echo "export CPLUS_INCLUDE_PATH=$HOME/opt/moab/include:\$CPLUS_INCLUDE_PATH" >> ~/.bashrc
-    echo "export C_INCLUDE_PATH=$HOME/opt/moab/include:\$C_INCLUDE_PATH" >> ~/.bashrc
-    cd ../../
+    export LD_LIBRARY_PATH=$install_dir/moab/lib:$LD_LIBRARY_PATH
+    export LIBRARY_PATH=$install_dir/moab/lib:$LIBRARY_PATH
+    echo "export LD_LIBRARY_PATH=$install_dir/moab/lib:\$LD_LIBRARY_PATH" >> ~/.bashrc
+    echo "export LIBRARY_PATH=$install_dir/moab/lib:\$LIBRARY_PATH" >> ~/.bashrc
+    echo "export CPLUS_INCLUDE_PATH=$install_dir/moab/include:\$CPLUS_INCLUDE_PATH" >> ~/.bashrc
+    echo "export C_INCLUDE_PATH=$install_dir/moab/include:\$C_INCLUDE_PATH" >> ~/.bashrc
 }
 
 
 function build_pytaps {
 
+    cd $install_dir
     # Install PyTAPS
     wget https://pypi.python.org/packages/source/P/PyTAPS/PyTAPS-1.4.tar.gz
     tar zxvf PyTAPS-1.4.tar.gz
     rm PyTAPS-1.4.tar.gz
     cd PyTAPS-1.4/
-    python setup.py --iMesh-path=$HOME/opt/moab --without-iRel --without-iGeom install --user
-    cd ..
+    python setup.py --iMesh-path=$install_dir/moab --without-iRel --without-iGeom install --user
 
 }
 
 function install_pyne {
 
-# Install PyNE
+    # Install PyNE
+    cd $install_dir
+    check_repo pyne
     git clone https://github.com/pyne/pyne.git
     cd pyne
-    python setup.py install --user -- -DMOAB_LIBRARY=$HOME/opt/moab/lib -DMOAB_INCLUDE_DIR=$HOME/opt/moab/include
+    python setup.py install --user -- -DMOAB_LIBRARY=$install_dir/moab/lib -DMOAB_INCLUDE_DIR=$install_dir/moab/include
     echo "export PATH=$HOME/.local/bin:\$PATH" >> ~/.bashrc
     echo "export LD_LIBRARY_PATH=$HOME/.local/lib:\$LD_LIBRARY_PATH" >> ~/.bashrc
-    echo "alias build_pyne='python setup.py install --user -- -DMOAB_LIBRARY=\$HOME/opt/moab/lib -DMOAB_INCLUDE_DIR=\$HOME/opt/moab/include'" >> ~/.bashrc
+    echo "alias build_pyne='python setup.py install --user -- -DMOAB_LIBRARY=$install_dir/moab/lib -DMOAB_INCLUDE_DIR=$install_dir/moab/include'" >> ~/.bashrc
 
 }
 
@@ -84,14 +88,13 @@ IFS=$'\n\t'
 # system update
 eval apt-get install -y $package_list
 
+install_dir=$HOME/opt
+mkdir -p $install_dir
+
 # need to put libhdf5.so on LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$hdf5_libdir
 export LIBRARY_PATH=$hdf5_libdir
 echo "export LD_LIBRARY_PATH=$hdf5_libdir" >> ~/.bashrc
-
-cd $HOME
-mkdir -p opt
-cd opt
 
 build_moab
 
@@ -99,4 +102,4 @@ build_pytaps
 
 install_pyne
 
-echo "PyNE build complete. PyNE can be rebuilt with the alias 'build_pyne' executed from $HOME/opt/pyne"
+echo "PyNE build complete. PyNE can be rebuilt with the alias 'build_pyne' executed from $install_dir/pyne"
