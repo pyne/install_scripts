beacb9ff5431e84ab2ee56adb04f79db49a1e671
Mongwell <19lion@gmail.com>
2019-06-19 12:49:06 -0500

    fixed comments on 18.04 script

--- ubuntu_18.04.sh
+++ ubuntu_18.04.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # This script builds the repo version of PyNE (with the MOAB optional
-# dependency) from scratch on Ubuntu 16.04. The folder $HOME/opt is created
+# dependency) from scratch on Ubuntu 18.04. The folder $HOME/opt is created
 # and PyNE is installed within.
 #
 # Run this script from any directory by issuing the command where <version>
 # is either "dev" or "stable":
-# $ ./ubuntu_16.04.sh <version>
+# $ ./ubuntu_18.04.sh <version>
 # After the build finishes run:
 #  $ source ~/.bashrc
 # or open a new terminal.
