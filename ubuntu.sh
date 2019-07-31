7c20921e638a77e4af9068d5dc18d05f4e7f0004
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2019-07-31 11:38:46 -0500

    adding functions

--- ubuntu.sh
+++ ubuntu.sh
@@ -16,70 +16,83 @@
 
 # Use package manager for as many packages as possible
 
+
+# process the arguments
+function proccess_args() {
+    if [ $# -lt 2 ]
+    then 
+        pyne_version=$1
+        detect_version
+    elif [ $# -eq 2 ]
+    then
+        pyne_version=$1
+        ubuntu_version=$2
+    else
+        echo "To many arguments provided. This script only accepts 2 \
+optionnal arguments:
+  1. the version of PyNE you wish to install: \"dev\" or \"stable\"
+  2. the Ubuntu version you are using: \"16.04\" or \"18.04\""
+        exit 1
+    fi
+}
+
+
 # Dectect Ubuntu version, requires lbs_core package to be installed
 function detect_version() {
-    local u_version=`lsb_release -r -s`
-    if [ -z "$u_version" ]
+    ubuntu_version=`lsb_release -r -s`
+    if [ -z "$ubuntu_version" ]
     then
-        echo "Can't detect your Ubuntu version, install 'lbs_core' from apt-get or
-        provide your Ubuntu version as a argument to this script (16.04 or
-        18.04)"
+        echo "Can't detect your Ubuntu version, install 'lbs_core' from \
+apt-get or provide your Ubuntu version as the second argument to this script \
+(16.04 or 18.04)"
     fi
-    return $u_version
 }
+
 
 # Check if the Ubuntu version is a supported one
 function validate_version() {
     local arg1=$1
     if [[ $arg1 == "" ]]
     then
-        echo "Unable to determine the system Ubuntu version, using the default
-        one: 18.04"    
+        echo "Unable to determine the system Ubuntu version, using the default \
+one: 18.04"    
     elif [[ $arg1 != "16.04" ]] && [[ $arg1 != "18.04" ]]
     then
-        echo "Only Ubuntu 16.06 and 18.04 are supported by this script use at your
-        own risk!"
+        echo "Only Ubuntu 16.06 and 18.04 are supported by this script. Use at \
+your own risk!"
     fi
 }
 
+
+# Set the base config
 function set_base_config() {
-apt_package_list="software-properties-common wget \
-             build-essential git cmake vim emacs gfortran libblas-dev \
-             python-pip liblapack-dev libhdf5-dev autoconf libtool"
-pip_package_list="numpy scipy cython nose tables matplotlib jinja2 \
-                  setuptools"
-hdf5_libdir=/usr/lib/x86_64-linux-gnu/hdf5/serial
+    apt_package_list="software-properties-common wget \
+                      build-essential git cmake vim emacs gfortran libblas-dev \
+                      python-pip liblapack-dev libhdf5-dev autoconf libtool"
+    pip_package_list="numpy scipy cython nose tables matplotlib jinja2 \
+                      setuptools"
+    hdf5_libdir="/usr/lib/x86_64-linux-gnu/hdf5/serial"
 }
 
+
+# Upate the configuration based on the ubuntu version
 function update_config() {
     local arg1=$1
     if [[ ${arg1} == "16.04" ]]
     then
-        apt_package_list="${apt_package_list} python-software-properties"
+        apt_package_list="python-software-properties ${apt_package_list}"
     fi
 }
 
-function proccess_args() {
-    if [ $# -lt 2 ]
-    then 
-        pyne_version=$1
-        version=$(detect_version)
-    elif [ $# -eq 2 ]
-    then
-        pyne_version=$1
-        version=$2
-    else
-        echo "To many argument provided. this script can only have take 2 optionnal
-        argument:\n
-        - the version of PyNE you wish to install: \"dev\" or \"stable\"\n
-        - the Ubuntu version you are using: \"16.04\" or \"18.04\""
-        exit 1
-    fi
-}
-
+## Main 
+# process the arguments
 proccess_args $*
-validate_version ${version}
+# Check is the Ubuntu version is a supported one
+validate_version ${ubuntu_version}
+
+# Set the base config
 set_base_config
-update_config ${version}
+# Upate the configuration based on the ubuntu version
+update_config ${ubuntu_version}
 
 source Ubuntu_mint.sh $pyne_version
