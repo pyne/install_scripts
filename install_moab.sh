50db45f9dbba0ddf3c1fb5c9c0b28dcde3ce995f
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2020-08-17 21:10:27 -0500

    how about this one?

--- install_moab.sh
+++ install_moab.sh
@@ -30,13 +30,13 @@ make
 make install
 
 # Adding MOAB/lib to $LD_LIBRARY_PATH and $LIBRARY_PATH
-add_export_var_to_bashrc '$'"LD_LIBRARY_PATH" "${install_dir}/moab/lib"
-add_export_var_to_bashrc '$'"LIBRARY_PATH" "${install_dir}/moab/lib"
+add_export_var_to_bashrc '\$LD_LIBRARY_PATH' "${install_dir}/moab/lib"
+add_export_var_to_bashrc '\$LIBRARY_PATH' "${install_dir}/moab/lib"
 
 # Adding MOAB/include to $CPLUS_INCLUDE_PATH and $_INCLUDE_PATH
-add_export_var_to_bashrc '$'"CPLUS_INCLUDE_PATH" "${install_dir}/moab/include"
-add_export_var_to_bashrc '$'"C_INCLUDE_PATH" "${install_dir}/moab/include"
+add_export_var_to_bashrc '\$CPLUS_INCLUDE_PATH' "${install_dir}/moab/include"
+add_export_var_to_bashrc '\$C_INCLUDE_PATH' "${install_dir}/moab/include"
 
 # Adding pymoab to $PYTHONPATH
 PYTHON_VERSION=$(python -c 'import sys; print(sys.version.split('')[0][0:3])')
-add_export_var_to_bashrc '$'"PYTHONPATH" "$install_dir/moab/lib/python${PYTHON_VERSION}/site-packages"
+add_export_var_to_bashrc '\$PYTHONPATH' "$install_dir/moab/lib/python${PYTHON_VERSION}/site-packages"
