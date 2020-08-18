cd082623528771b4b707f71046cbb2e813776515
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2020-08-17 21:49:20 -0500

    readding pyne

--- ubuntu_mint.sh
+++ ubuntu_mint.sh
@@ -19,4 +19,6 @@ echo "export LIBRARY_PATH=${hdf5_libdir}" >> ~/.bashrc
 
 ./install_dagmc.sh ${install_dir}
 
+./install_pyne.sh
+
 echo "Run 'source ~/.bashrc' to update environment variables. PyNE may not function correctly without doing so."
